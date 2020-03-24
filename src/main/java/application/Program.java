3149d6d87f977ec954ef0527c742900847513543
alexandremolica <alexandremolica@gmail.com>
2020-03-24 17:52:42 -0300

    Projeto criado

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+//		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+//		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+//		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+ 		//insert
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+	
+	
+		//select
+//		Pessoa p = em.find(Pessoa.class, 2);
+//		System.out.println(p);
+
+		//delete
+		Pessoa p = em.find(Pessoa.class, 4);
+
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+
+	}
+
+}
